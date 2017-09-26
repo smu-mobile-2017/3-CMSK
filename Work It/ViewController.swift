312bbf2a42586f8d38da7377f3757ecb80dc10a8
phrz <phrz@me.com>
2017-09-25 23:48:42 -0500

    Initial

---
+++ Work It/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Work It
+//
+//  Created by Paul Herz on 9/25/17.
+//  Copyright © 2017 Paul Herz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
